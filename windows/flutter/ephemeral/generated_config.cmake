a8103c7ac5e36f8010e716b676cdcf3ef12157be
UcGeorge <petegeorge20005@gmail.com>
2021-05-08 14:06:29 +0100

    Added android, ios and web files

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Xampp\\htdocs\\flutter_spotify_ui" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=D:\\Xampp\\htdocs\\flutter_spotify_ui"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Xampp\\htdocs\\flutter_spotify_ui\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Xampp\\htdocs\\flutter_spotify_ui"
+  "FLUTTER_TARGET=D:\\Xampp\\htdocs\\flutter_spotify_ui\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Xampp\\htdocs\\flutter_spotify_ui\\.dart_tool\\package_config.json"
+)
